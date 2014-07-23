c99e190b99dfd05047fad8e0ab209f28d89a1a0d
sethmatics <sethmatics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 02:41:17 +0000

    protect admin class from direct access
    git-svn-id: https://plugins.svn.wordpress.org/sm-sticky-clicky-star/trunk@953181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sm-sticky-clicky-star-admin.php
+++ sm-sticky-clicky-star-admin.php
@@ -1,4 +1,9 @@
 <?php
+//avoid direct calls to this file, because now WP core and framework has been used
+if ( ! function_exists( 'add_filter' ) ) {
+	header('Status: 403 Forbidden'); header('HTTP/1.1 403 Forbidden'); exit();
+}
+
 if(!class_exists('sm_sticky_clicky_star_admin'))
 {
 	class sm_sticky_clicky_star_admin
