9a08f832c6031af6a020e9786d01118e212a4e06
sethmatics <sethmatics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 02:37:07 +0000

    converted plugin to use php classes, and made scripts load more efficiently
    git-svn-id: https://plugins.svn.wordpress.org/sm-sticky-clicky-star/trunk@953179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sm-sticky-clicky-star-admin.php
@@ -0,0 +1,110 @@
+<?php
+if(!class_exists('sm_sticky_clicky_star_admin'))
+{
+	class sm_sticky_clicky_star_admin
+	{
+		public $installed_dir;
+		public $installed_url;
+
+		public function __construct($plugin)
+		{
+			$this->installed_dir = $plugin->installed_dir;
+			$this->installed_url = $plugin->installed_url;
+
+			add_action('post_submitbox_misc_actions', array($this, 'sticky_meta'));
+			add_filter('manage_posts_columns', array($this,'add_sticky_column'));
+			//adds sticky star to appthemes themes
+			if(defined('APP_POST_TYPE')){
+				add_filter('manage_edit-'.APP_POST_TYPE.'_columns',array($this, 'add_sticky_column'));
+			}
+			add_action('manage_posts_custom_column',  array($this,'sticky_column_content'));
+			add_action('wp_ajax_sm_sticky', array($this,'sticky_callback'));
+
+			//load style and js on pages that need it, note that admin_enqueue_styles does not work as of WP 4.0.0
+			add_action('admin_enqueue_scripts', array($this,'click_to_stick_styles'));
+			add_action('admin_enqueue_scripts', array($this,'click_to_stick_scripts'));
+		}
+
+		function add_sticky_column ($columns) {
+			$columns['sticky'] = 'Sticky';
+			return $columns;
+		}
+
+		//add admin stylesheet
+		function click_to_stick_styles($hook) {
+			if( $hook == 'edit.php' || $hook == 'post.php') {
+				wp_enqueue_style('sm_click_to_stick_styles', $this->installed_url.'css/sm-click-to-stick.css', array(), '1.0.0', 'all');
+			}
+		}
+
+		// add admin javascript
+		function click_to_stick_scripts($hook) {
+			if( $hook == 'edit.php' || $hook == 'post.php') {
+				wp_enqueue_script( 'sm_click_to_stick_scripts', $this->installed_url.'js/sm-click-to-stick.js', array('jquery') );
+			}
+		}
+
+		function sticky_meta() {
+			global $post;
+			if($post->post_type !='page')
+				echo '<div id="smSticky" class="misc-pub-section ">Make Sticky: '.$this->get_sticky_link($post->ID).'</div>';
+		}
+
+		function sticky_column_content($name) {
+			global $post;
+			if($name=='sticky')
+				echo $this->get_sticky_link($post->ID);
+		}
+
+		function get_sticky_link($thePostID = '') {
+			global $post;
+			if($thePostID == '')
+				$thePostID = $post->ID;
+			$stickyClass = '';
+			$stickyTitle = 'Make Sticky';
+			if(is_sticky($thePostID)) {
+				$stickyClass = 'isSticky';
+				$stickyTitle = 'Remove Sticky';
+			}
+			$stickyLink = '<a href="id='.$thePostID.'&code='.wp_create_nonce('sm-sticky-nonce').'" id="smClickToStick'.$thePostID.'" class="smClickToStick '.$stickyClass.'" title="'.$stickyTitle.'"></a>';
+			return $stickyLink;
+		}
+
+		function sticky_callback() {
+			if ( !wp_verify_nonce( $_POST['code'], 'sm-sticky-nonce' ) ) {
+				// failed nonce validation
+				echo 'failed nonce: '.$_POST['anthem_nonce'];die();
+			}
+
+			$stickyPosts = get_option('sticky_posts');
+
+			if(!is_array($stickyPosts))
+				$stickyPosts = array();
+
+			if (in_array($_POST['id'], $stickyPosts)) {
+				$removeKey = array_search($_POST['id'], $stickyPosts);
+				unset($stickyPosts[$removeKey]);
+				$stickyResult = 'removed';
+			}
+			else {
+				array_unshift($stickyPosts, $_POST['id']);
+				//$stickyPost[] = $_POST['id'];
+				$stickyResult = 'added';
+			}
+
+			if(update_option('sticky_posts', $stickyPosts))
+				echo $stickyResult;
+			else
+				echo 'An error occured';
+
+			die(); // this is required to return a proper result
+		}
+	}
+}
+
+
+
+
+
+
+
--- sm-sticky-clicky-star.php
+++ sm-sticky-clicky-star.php
@@ -1,33 +1,85 @@
 <?php
-/*
-Plugin Name: SM Sticky Clicky Star
-Plugin URI: http://sethmatics.com/extend/plugins/sm-sticky-clicky-star/
-Description: Turn sticky (featured) posts on and off with 1 easy click! Control permissions with "User Role Editor".
-Author: sethmatics, bigj9901
-Version: 1.0.7
-Author URI: http://sethamtics.com/
-License: GNU General Public License v2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: sm_sticky_clicky_star
-*/
+/**
+ * Plugin Name: SM Sticky Clicky Star
+ * Plugin URI: http://sethmatics.com/extend/plugins/sm-sticky-clicky-star/
+ * Description: Turn sticky (featured) posts on and off with 1 easy click! Control permissions with "User Role Editor".
+ * Author: sethmatics, bigj9901
+ * Version: 1.0.7
+ * Author URI: http://sethamtics.com/
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: sm_sticky_clicky_star
+ *
+ * @package sm-sticky-clicky-star
+ * @category plugin
+ * @author sethmatics, bigj9901
+ */
 
 //avoid direct calls to this file, because now WP core and framework has been used
 if ( ! function_exists( 'add_filter' ) ) {
-	header('Status: 403 Forbidden');
-	header('HTTP/1.1 403 Forbidden');
-	exit();
+	header('Status: 403 Forbidden'); header('HTTP/1.1 403 Forbidden'); exit();
 }
 
-define('sm_sticky_clicky_star_DIR', WP_PLUGIN_DIR.'/sm-sticky-clicky-star/');
+if(!class_exists('sm_sticky_clicky_star'))
+{
+	class sm_sticky_clicky_star
+	{
+		public $installed_dir;
+		public $installed_url;
 
-// get the "author" role object
-$role = get_role( 'administrator' );
-$role->add_cap( 'edit_post_sticky' );
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct()
+		{
+			$this->installed_dir = dirname(__FILE__);
+			$this->installed_url = plugins_url('/', __FILE__);
+			//only load the sticky clicky star plugin functions if the user has the capability to "sticky" posts.
+			add_action('init', array($this,'init'));
+		} // END public function __construct
 
-//only load the sticky clicky star plugin functions if the user has the capability to "sticky" posts.
-add_action('init', 'sm_sticky_clicky_star_init');
-function sm_sticky_clicky_star_init() {
-	if(current_user_can('edit_others_posts') || current_user_can('edit_post_sticky') ) {
-		require_once(sm_sticky_clicky_star_DIR.'sm-sticky-clicky-star-functions.php');
-	}
+		/**
+		 * Initialize the plugin -either for admin (back end) or public (front end)
+		 */
+		function init() {
+
+			if(current_user_can('edit_others_posts') || current_user_can('edit_post_sticky') ) {
+				require_once($this->installed_dir.'/sm-sticky-clicky-star-admin.php');
+				new sm_sticky_clicky_star_admin($this);
+			}
+		}
+
+		/**
+		 * Activate the plugin
+		 */
+		public static function activate()
+		{
+			// get the "admin" role object and add capability
+			$role = get_role( 'administrator' );
+			$role->add_cap( 'edit_post_sticky' );
+		} // END public static function activate
+
+		/**
+		 * Deactivate the plugin
+		 */
+		public static function deactivate()
+		{
+			global $wp_roles;
+			foreach (array_keys($wp_roles->roles) as $role) {
+				$wp_roles->remove_cap($role, 'edit_post_sticky');
+			}
+		} // END public static function deactivate
+
+	} // END class sm_sticky_clicky_star
+} // END if(!class_exists('sm_sticky_clicky_star'))
+
+if(class_exists('sm_sticky_clicky_star'))
+{
+	// Installation and uninstallation hooks
+	register_activation_hook(__FILE__, array('sm_sticky_clicky_star', 'activate'));
+	register_deactivation_hook(__FILE__, array('sm_sticky_clicky_star', 'deactivate'));
+
+	// instantiate the plugin class
+	$sm_sticky_clicky_star = new sm_sticky_clicky_star();
 }
+
